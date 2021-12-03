f4a7a7eb02a5854be31c94157b70e171794174d3
wldnjs1277@naver.com <42260892+wldnjs1277@users.noreply.github.com>
2021-12-03 15:07:12 +0900

    -후위표기법 만들기

---
+++ app/src/main/java/com/example/calculator/test.kt
@@ -0,0 +1,32 @@
+package com.example.calculator
+
+import java.util.*
+import kotlin.collections.ArrayList
+
+
+fun main(){
+postfix("(3+4)*(1-2)")
+}
+//만드는중
+fun postfix(input:String){
+    var stack=Stack<String>()
+    var result=StringBuilder()  // StringBuilder 코틀린에서 동적으로 문자열을 조작가능
+    for(i in 0 until input.length){
+        when(input[i].toString()){
+            "(" -> {stack.push("(")}
+            ")" ->{if(stack.isEmpty()==false){
+                result.append(stack.pop()) }
+            }
+            "+" -> {stack.push("+")}
+            "-" -> {stack.push("-")}
+            "*" -> {stack.push("*")}
+            "/" -> {stack.push("/")}
+            else -> result.append(input[i].toString())
+        }
+
+    }
+    println(result)
+}
+
+
+
