cc11dcc1730b55fb863be2f5f33e7103bfe0c7ca
wldnjs1277@naver.com <42260892+wldnjs1277@users.noreply.github.com>
2021-12-05 01:05:46 +0900

    -후위표기법만들기

--- app/src/main/java/com/example/calculator/test.kt
+++ app/src/main/java/com/example/calculator/test.kt
@@ -1,30 +1,48 @@
 package com.example.calculator
 
 import java.util.*
-import kotlin.collections.ArrayList
+
 
 
 fun main(){
 postfix("(3+4)*(1-2)")
 }
 //만드는중
-fun postfix(input:String){
-    var stack=Stack<String>()
-    var result=StringBuilder()  // StringBuilder 코틀린에서 동적으로 문자열을 조작가능
-    for(i in 0 until input.length){
-        when(input[i].toString()){
-            "(" -> {stack.push("(")}
-            ")" ->{if(!stack.isEmpty()){
-                result.append(stack.pop()) }
+fun postfix(input:String) {
+    var stack = Stack<String>()
+    var result = StringBuilder()  // StringBuilder 코틀린에서 동적으로 문자열을 조작가능
+    for (i in 0 until input.length) {
+        when (input[i].toString()) {
+            "(" -> {
+                stack.push("(")
             }
-            "+" -> {stack.push("+")}
-            "-" -> {stack.push("-")}
-            "*" -> {stack.push("*")}
-            "/" -> {stack.push("/")}
+            ")" -> {
+                if (!stack.isEmpty()) {
+                    for (j in 0 until stack.size) {
+                        //result.append(stack.pop())
+                    }
+                }
+            }
+            "+" -> {
+                stack.push("+")
+            }
+            "-" -> {
+                stack.push("-")
+            }
+            "*" -> {
+                stack.push("*")
+            }
+            "/" -> {
+                stack.push("/")
+            }
             else -> result.append(input[i].toString())
         }
-
     }
+    for(k in 0 until stack.size){
+        result.append(stack.pop())
+    }
+
+
     println(result)
 }
 
