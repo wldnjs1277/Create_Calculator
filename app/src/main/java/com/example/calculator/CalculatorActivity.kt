cf187f3bc9049546b807729b19fe4854c8e5f415
wldnjs1277@naver.com <42260892+wldnjs1277@users.noreply.github.com>
2022-01-04 14:51:29 +0900

    -정리하기

--- app/src/main/java/com/example/calculator/CalculatorActivity.kt
+++ app/src/main/java/com/example/calculator/CalculatorActivity.kt
@@ -33,7 +33,7 @@ class CalculatorActivity : AppCompatActivity() {
             binding.OneBtn, binding.TwoBtn, binding.ThreeBtn,
             binding.FourBtn, binding.FiveBtn, binding.SixBtn,
             binding.SevenBtn, binding.EightBtn, binding.NineBtn
-        //listof.forEach listof에 있는 값을 button으로 받아 반복문 수행
+        //listof.forEach listof()에 있는 값을 button으로 받아 반복문 수행
         ).forEach { button ->
             //setOnClickListener 클릭시 수행할동작을 지정한다.
             //버튼이 클릭되었을때 수행할 동작
@@ -54,11 +54,15 @@ class CalculatorActivity : AppCompatActivity() {
         //list를 생성하고 ()의 값으로 초기화
         listOf(
             binding.PlusBtn, binding.MinusBtn, binding.Multiplicaton, binding.DivisonBtn, binding.percent
+        //listof()를 button으로 받아 반복문 수행
         ).forEach { button ->
             button.setOnClickListener {
+                //expstr=addop()의 결과를 할당
                 expStr = addOp(
                     oldExpStr = expStr,
+                    //newNumberStr = binding.textView.text를 string으로 형변환 한 값
                     newNumberStr = binding.textView.text.toString(),
+                    //newOpStr = button.text를 string으로 형변환 한 값
                     newOpStr = button.text.toString()
                 )
                 //binding.textView.text는 getZeroStr()결과를 할당
@@ -69,31 +73,42 @@ class CalculatorActivity : AppCompatActivity() {
 
     /* 숫자와 수식이 아닌 버튼 클릭리스너 */
     private fun otherClickListener() {
+        //binding.PMBtn이 클릭되었을때 수행할 종작
         binding.PMBtn.setOnClickListener {
+            //binding.textView.text는 changeDataMask()결과를 할당
             binding.textView.text = changeDataMask(
+                // newNumberStr = binding.textView.text를 string으로 형변환 한 값
                 newNumberStr = binding.textView.text.toString()
             )
         }
-
+        //binding.DecimalPoint이 클릭되었을때 수행할 동작
         binding.DecimalPoint.setOnClickListener {
+            //binding.textView.text는 changeDataPoint()결과를 할당
             binding.textView.text = changeDataPoint(
+                //newNumberStr = binding.textView.text를 string으로 형변환 한 값
                 newNumberStr = binding.textView.text.toString()
             )
         }
-
+        //binding.ACBtn이 클릭되었을때 수행할 동작
         binding.ACBtn.setOnClickListener {
+            //expStr은 getEmptyStr()의 결과를 할당
             expStr = getEmptyStr()
+            //binding.textView.text은 getZeroStr()결과를 할당
             binding.textView.text = getZeroStr()
         }
-
+        //binding.Equals이 클릭되었을때 수행할 동작
         binding.Equals.setOnClickListener {
+            //expStr은 addOp()결과를 할당
             expStr = addOp(
                 oldExpStr = expStr,
+                //newNumberStr = binding.textView.text를 string으로 형변환 한 값
                 newNumberStr = binding.textView.text.toString()
             )
+            //binding.textView.text는 getCalcResult()결과를 할당
             binding.textView.text = getCalcResult(
                 expStr = expStr
             )
+            //expStr은 getEmptyStr()결과를 할당
             expStr = getEmptyStr()
         }
     }
